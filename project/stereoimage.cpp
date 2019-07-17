dcd23e5777705aedee7475df4af2c89738590e96
RaphaelWW <raphael.wild@tum.de>
2019-07-17 12:35:45 +0200

    Add logarithmic scaling

--- project/stereoimage.cpp
+++ project/stereoimage.cpp
@@ -231,7 +231,7 @@ void StereoImage::disparityToDepth()
 #pragma omp parallel for
     for(int i = 0; i< m_leftImageWidth * m_leftImageHeight; i++)
     {
-        m_depthImageRectified[i] = baseline * focalLength / m_disparity[i];
+        m_depthImageRectified[i] = log(baseline * focalLength / m_disparity[i]);
     }
 }
 
