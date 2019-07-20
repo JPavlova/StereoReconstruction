581f6ce24aff380697af0ed65332d14badcefef0
Lukas Knak <ge25vub@tum.de>
2019-07-20 23:32:25 +0200

    create plots for relative quantitative comparison of patchmatch and blockmatch

--- project/python/s.py
+++ project/python/s.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os, fnmatch
 
 
 def trya():
@@ -11,16 +12,11 @@ def trya():
     plt.vlines(threshold, ymin=0, ymax=255, colors="red")
     plt.show()
 
-def disparity(threshold, filename, title, savef=False):
-
-    min_ = 0
-    # opencv 3500, pm/bm 40000
-    max_ = threshold
-
+def read_file(filename, invalid):
     with open(filename, "r") as file:
         xml = file.read().splitlines()
 
-    disparity_map = []
+    map = []
     for element in xml:
         temp = []
         # print(element)
@@ -28,13 +24,21 @@ def disparity(threshold, filename, title, savef=False):
             # print(el)
             if el in ["\n", ""]:
                 continue
-            if el != "-inf" and el != "inf":
-                temp.append(abs(float(el)))
-            else:
+            if el in invalid:
                 temp.append(np.nan)
-        disparity_map.append(temp)
+            else:
+                temp.append(abs(float(el)))
+        map.append(temp)
 
-    disparity_map = np.array(disparity_map)
+    return np.array(map)
+
+def disparity(threshold, filename, title, savef=False):
+
+    min_ = 0
+    # opencv 3500, pm/bm 40000
+    max_ = threshold
+
+    disparity_map = read_file(filename, ["-2147483648", "2147483648"])
 
     histogram_array = disparity_map[~np.isnan(disparity_map)].reshape(-1)
     #print(histogram_array, histogram_array.shape)
@@ -74,24 +78,7 @@ def depth(threshold, filename, title, savef=False):
     # opencv 3500, pm/bm 40000
     max_ = threshold
 
-    with open(filename, "r") as file:
-        xml = file.read().splitlines()
-
-    depth_values = []
-    for element in xml:
-        temp = []
-        # print(element)
-        for el in element.split(sep="\t"):
-            # print(el)
-            if el in ["\n", ""]:
-                continue
-            if el != "-inf" and el != "inf":
-                temp.append(float(el))
-            else:
-                temp.append(np.nan)
-        depth_values.append(temp)
-
-    depth_values = np.array(depth_values)
+    depth_values = read_file(filename, ["-inf", "inf"])
 
     histogram_array = depth_values[~np.isnan(depth_values)].reshape(-1)
     #print(histogram_array, histogram_array.shape)
@@ -125,12 +112,101 @@ def depth(threshold, filename, title, savef=False):
     if savef: plt.savefig(fname=str(title) + ".png")
     plt.show()
 
+def h_fun_disp(hist_array, min_, max_, title, save=False, filename=None):
+    hist, bins = np.histogram(hist_array, bins=np.arange(min_, max_, (max_ - min_) / 50))
+    hist_normalized = hist / hist_array.shape[0] * 100
+    centers = (bins[:-1] + bins[1:]) / 2
+    bin_size = bins[1]-bins[0]
+
+    plt.bar(centers, hist_normalized, width=bin_size)
+
+    highest = np.argmax(hist_normalized)
+    second = highest + 1 if highest == 0 else highest - 1
+    plt.hlines(hist_normalized[highest], centers[second], centers[highest], color='orange')
+    plt.text(centers[second], hist_normalized[highest], '{:.2f}'.format(hist_normalized[highest]),
+             ha="left" if highest == 0 else "right", va='center', color='orange')
+
+    plt.ylim(0, 100)
+    plt.title(title, y=1.03)
+    plt.xlabel(f"Disparity differences (in pixels), bin size: {max(int(bin_size), 1)}")
+    plt.ylabel("Percent pixels contained in bin")
+    if(save): plt.savefig(filename + f"_{int(min_)}_{int(max_)}.png")
+    plt.show()
+
+def h_fun_depth(hist_array, min_, max_, title, save=False, filename=None):
+    hist, bins = np.histogram(hist_array, bins=np.arange(min_, max_, (max_ - min_) / 50))
+    hist_normalized = hist / hist_array.shape[0] * 100
+    centers = (bins[:-1] + bins[1:]) / 2
+    bin_size = bins[1]-bins[0]
+
+    plt.bar(centers, hist_normalized, width=bin_size)
+
+    highest = np.argmax(hist_normalized)
+    second = highest + 1 if highest==0 else highest - 1
+    plt.hlines(hist_normalized[highest], centers[second], centers[highest], color='orange')
+    plt.text(centers[second], hist_normalized[highest], '{:.2f}'.format(hist_normalized[highest]), ha="left" if highest==0 else "right", va='center',color='orange')
+
+    plt.ylim(0, 100)
+    plt.title(title, y=1.03)
+    plt.xlabel(f"Depth differences (in mm), bin size: {max(int(bin_size), 1)}")
+    plt.ylabel("Percent pixels contained in bin")
+    if(save): plt.savefig(filename + f"_{int(min_)}_{int(max_)}.png")
+    plt.show()
+
+def compare(path, bsize, type_, title, save, filename):
+    histogram_ = False
+    invalid = ["-inf", "inf"] if type_ == "depth" else ["-2147483648", "2147483648"]
+    for root, dir, files in os.walk(path):
+        if f"patchmatch_{type_}_values{bsize}.txt" in files and f"blockmatch_{type_}_values{bsize}.txt" in files:
+            print("Reading in...\t" + root)
+            depth_vals_1 = read_file(root + f"/patchmatch_{type_}_values{bsize}.txt", invalid)
+            depth_vals_2 = read_file(root + f"/blockmatch_{type_}_values{bsize}.txt", invalid)
+            print("Depths", depth_vals_1, depth_vals_2)
+
+            overlap = ((~np.isnan(depth_vals_1)).astype(int) + (~np.isnan(depth_vals_2)).astype(int)) > 0
+            print("OVERLAP", overlap)
+            print("VALUES:", depth_vals_1[overlap], depth_vals_2[overlap])
+            diff = depth_vals_1[overlap == True] - depth_vals_2[overlap == True]
+            diff = diff[~np.isnan(diff)]
+            diff = np.abs(diff[np.abs(diff) < 10000]) if type_ == "depth" else diff
+            print("Individual values: " + str(diff.reshape(-1).shape[0]))
+
+            if not(histogram_):
+                histogram_array = diff.reshape(-1)
+                histogram_ = True
+            else:
+                if diff.shape[0] > 0:
+                    histogram_array = np.concatenate([histogram_array, diff.reshape(-1)], axis=0)
+
+    print(histogram_array.shape[0])
+
+    min_ = np.min(histogram_array)
+    max_ = np.max(histogram_array)
+    print(np.sum(np.isnan(histogram_array)))
+
+    if type_ == "depth":
+        h_fun_depth(histogram_array, min_, max_, title, save, filename)
+        h_fun_depth(histogram_array, 0, 2000, title, save, filename)
+        h_fun_depth(histogram_array, 0, 100, title, save, filename)
+
+    else:
+        h_fun_disp(histogram_array, min_, max_, title, save, filename)
+        h_fun_disp(histogram_array, -50, 50, title, save, filename)
+        h_fun_disp(histogram_array, -25, 25, title, save, filename)
 
 if __name__ == "__main__":
 
-    bsize = 3
-    savef = False
-    path="../../build-project-Desktop-Debug/classroom_1deg/"
+    bsize = 11
+    savef = True
+    path="../../build-project-Desktop-Debug/"
+
+
+    #compare(path, bsize, "depth",
+    #        f"Histogram: Depth Map Comparison between\n Patchmatch and Blockmatch (Blocksize: {bsize})",
+    #        savef, f"HISTOGRAM_Depth_COMPARISON_{bsize}")
+
+    compare(path, bsize, "disparity", f"Histogram: Disparity Map Comparison between\n Patchmatch and Blockmatch (Blocksize: {bsize})",
+               savef, f"HISTOGRAM_DISPARITY_COMPARISON_{bsize}")
 
     # need to manually adjust scales so images look similar!
     # opencv is for whatever reason always smaller by some factor 10-20!
@@ -140,5 +216,5 @@ if __name__ == "__main__":
     #depth(100000, path + f"blockmatch_depth_values{bsize}.txt", f"BLOCKMATCH DEPTHMAP BLOCKSIZE {bsize}", savef)
 
     #disparity(1400, path + f"opencv_disparity_values{bsize}.txt", f"OPENCV DISPARITY MAP BLOCKSIZE {bsize}", savef)
-    disparity(100, path + f"patchmatch_disparity_values{bsize}.txt", f"PATCHMATCH DISPARITY MAP BLOCKSIZE {bsize}", savef)
-    disparity(100, path + f"blockmatch_disparity_values{bsize}.txt", f"BLOCKMATCH DISPARITY MAP BLOCKSIZE {bsize}", savef)
+    #disparity(100, path + f"patchmatch_disparity_values{bsize}.txt", f"PATCHMATCH DISPARITY MAP BLOCKSIZE {bsize}", savef)
+    #disparity(100, path + f"blockmatch_disparity_values{bsize}.txt", f"BLOCKMATCH DISPARITY MAP BLOCKSIZE {bsize}", savef)
